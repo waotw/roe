c15eb12d2d602d7f2cf36e09dde76b01c548e32d
benw <benjaminwelch@hey.com>
2026-03-23 11:09:26 -0500

    Add commented static site server to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
 
+  # Switch to serving static site files in production
+  # config.public_file_server.enabled = true
+
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
