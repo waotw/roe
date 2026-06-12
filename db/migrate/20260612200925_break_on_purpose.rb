ea973cc2f2269329eb50d3a72687cacdaaa6ebb5
benw <benjaminwelch@hey.com>
2026-06-12 17:31:47 -0500

    v0.0.15 - remove broken migration to test rolleback

--- db/migrate/20260612200925_break_on_purpose.rb
+++
@@ -1,6 +0,0 @@
-class BreakOnPurpose < ActiveRecord::Migration[8.0]
-  def change
-    # References a table that doesn't exist — db:migrate raises.
-    add_column :totally_nonexistent_table, :foo, :string
-  end
-end
