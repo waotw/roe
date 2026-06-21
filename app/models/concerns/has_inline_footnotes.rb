0d301644d8f72cbf62830c82145d4442af07fcc7
benw <benjaminwelch@hey.com>
2026-06-21 15:29:36 -0500

    fix(RoeAnji): retire inline footnote syntax, keep concern as no-op

--- app/models/concerns/has_inline_footnotes.rb
+++ app/models/concerns/has_inline_footnotes.rb
@@ -3,32 +3,11 @@ module HasInlineFootnotes
 
   private
 
+  # The (*...*) inline footnote syntax has been retired in favour of
+  # standard Kramdown footnotes ([^1] / [^1]:) and image captions
+  # (![alt](/path.jpg)(*caption*)). This method is kept as a no-op so
+  # existing include statements in models don't need to change.
   def process_inline_footnotes(markdown)
-    footnote_counter = 0
-    footnotes = []
-
-    # Match (*...*) or (*[marker] ...*)
-    processed = markdown.gsub(/\(\*(\[([^\]]+)\]\s*)?(.*?)\*\)/m) do
-      marker = $2
-      content = $3.strip
-
-      if marker
-        # Custom marker: (*[source] Author Name*)
-        footnotes << "[^#{marker}]: #{content}"
-        "[^#{marker}]"
-      else
-        # Auto-numbered: (*This is a footnote*)
-        footnote_counter += 1
-        footnotes << "[^#{footnote_counter}]: #{content}"
-        "[^#{footnote_counter}]"
-      end
-    end
-
-    # Append footnote definitions at end if any were found
-    if footnotes.any?
-      processed + "\n\n" + footnotes.join("\n")
-    else
-      processed
-    end
+    markdown
   end
 end
